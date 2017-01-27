5fd815b5a1a09e10e90df0774a5965e18bfeb7b3
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-27 16:36:49 +0800

    Update lattice.py

--- src/lattice.py
+++ src/lattice.py
@@ -1,2 +1,23 @@
 import numpy as np
 
+class lattice:
+    def __init__(self,dim,L):
+        self.dim=dim
+        self.L=L
+      
+    def lattice(self,init):
+        if init=="ColdStart":
+            return  
+        elif init=="HotStart":
+            return
+        else:
+            raise Exception("")
+            
+    def boundary_condition(self,site):
+        if (site<0):
+            site=self.L-1
+        elif(site>self.L-1):
+            site=0
+        return site
+    
+    
