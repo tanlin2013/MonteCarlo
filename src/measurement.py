ffb278733b3e7edb02d945f292d8d6c91f18b81b
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-27 23:26:48 +0800

    Update measurement.py

--- src/measurement.py
+++ src/measurement.py
@@ -1,3 +1,34 @@
 import numpy as np
+import lattice
 
+class physical:
+    def __init__(self,dim,L,ham):
+        self.dim=dim
+        self.L=L
+        self.ham=ham
+        self.state=state
+    
+    def total_energy(self):
+        
+        return E
+      
+    def magnetization(self):
+         
+        return m
+    
+    def HeatCapacity(self):
+        
+        return Cv
+  
+    def susceptibility(self):
+        
+        return chi
 
+class statistical:
+    def __init__(self):
+        self.state=state
+      
+    def AutoCorrelation(self):
+        
+        return gamma,tau
+  
