176113c9b6da2cbebcec3e2b3354fec2265149a4
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-27 17:56:30 +0800

    Update lattice.py

--- src/lattice.py
+++ src/lattice.py
@@ -4,12 +4,13 @@ class lattice:
     def __init__(self,dim,L):
         self.dim=dim
         self.L=L
-      
-    def lattice(self,init):
+        
+    def initialize_lattice(self,init):
+        size=tuple([L]*dim)
         if init=="ColdStart":
-            return  
+            return np.ones(size,dtype=int)  
         elif init=="HotStart":
-            return
+            return np.array((np.random.randint(0,2,size)-0.5)*2,dtype=int) 
         else:
             raise Exception("")
             
