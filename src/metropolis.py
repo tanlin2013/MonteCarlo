f86045b9273b2e0c916bab1adb0e416d38866190
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-31 16:57:19 +0800

    Update metropolis.py

--- src/metropolis.py
+++ src/metropolis.py
@@ -1,27 +1,25 @@
 import numpy as np
+import copy
 
 def sampling(lattice,ham,beta,init_state=None,Nflip=2,maxstep=1e+5,show_stats=True):
     time=0 ; histogram=[]
     if init_state is None:
         state=lattice.initialize_state("HotStart")
     while time < maxstep:
-        trail=0 ; trailist=[]
+        dE=0.0 ; trail=0 ; trailist=[]
         while trial < Nflip:
             site=np.random.randint(0,lattice.L,(lattice.dim,))
+            DM=copy.copy(lattice.domain)
+            flipState=np.random.choice(DM.remove(state[site]),1).item()
             if trail > 0 and site is in trailist:
                 trail-=1
-            else:    
-                state[site]=flip(state,site,lattice.domain)
-                trail+=1
-        dE=   
-        if ((dE>0.0)and(np.random.random()<=np.exp(-self.beta*dE))):
-            for site in trailist:
-                state[site]=flip(state,site,lattice.domain)
+            else:
+                dE+=ham(flipState,site)-ham(state,site)    
+                trailist.append((site,flipState)) ; trail+=1
+        if not ((dE>0.0)and(np.random.random()>=np.exp(-beta*dE))):
+            for site,flipState in trailist:
+                state[site]=flipState
         histogram.append(state) ; time+=1
         if show_stats and time%(maxstep/100)==0:
             print "beta=%.3f, thermalize stage: %.f%%" %(beta,100*(time/float(maxstep)))
     return histogram
-
-def flip(state,site,domain):
-    
-    return state
