88b1d4c50b71e58ef9b07a4f96daa8e2f9a82ef2
Tan Tao-Lin <tanlin2013@gmail.com>
2017-01-31 15:30:50 +0800

    Update metropolis.py

--- src/metropolis.py
+++ src/metropolis.py
@@ -1,7 +1,27 @@
 import numpy as np
 
-def flip(dE,beta):
-    if not((dE>0.0)and(np.random.random()>=np.exp(-self.beta*dE))):
-        return True
-    else:
-        return False
+def sampling(lattice,ham,beta,init_state=None,Nflip=2,maxstep=1e+5,show_stats=True):
+    time=0 ; histogram=[]
+    if init_state is None:
+        state=lattice.initialize_state("HotStart")
+    while time < maxstep:
+        trail=0 ; trailist=[]
+        while trial < Nflip:
+            site=np.random.randint(0,lattice.L,(lattice.dim,))
+            if trail > 0 and site is in trailist:
+                trail-=1
+            else:    
+                state[site]=flip(state,site,lattice.domain)
+                trail+=1
+        dE=   
+        if ((dE>0.0)and(np.random.random()<=np.exp(-self.beta*dE))):
+            for site in trailist:
+                state[site]=flip(state,site,lattice.domain)
+        histogram.append(state) ; time+=1
+        if show_stats and time%(maxstep/100)==0:
+            print "beta=%.3f, thermalize stage: %.f%%" %(beta,100*(time/float(maxstep)))
+    return histogram
+
+def flip(state,site,domain):
+    
+    return state
