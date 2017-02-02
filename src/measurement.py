1309a24307001473941d18a221818132bf935760
Tan Tao-Lin <tanlin2013@gmail.com>
2017-02-02 11:08:51 +0800

    Update measurement.py

--- src/measurement.py
+++ src/measurement.py
@@ -1,25 +1,26 @@
 import numpy as np
 
 class physical:
-    def __init__(self,lattice,ham,state):
+    def __init__(self,lattice,ham,state,kb):
         self.L=lattice.L
         self.dim=lattice.dim
         self.ham=ham
         self.state=state
+        self.kb=kb
     
-    def energy(self):
+    def energy(self,beta):
         U=0.0 ; U2=0.0 ; size=tuple([self.L]*self.dim)
         for site in np.ndindex(size):
             U+=0.5*self.ham(self.state,site)
             U2+=U**2
-        E=U/float(self.L**self.dim)
-        Cv=U2/float(self.L**self.dim)-E**2
+        E=U/float(self.L**self.dim) ; E2=U2/float(self.L**self.dim)
+        Cv=self.kb*beta**2*(E2-E**2)
         return E,Cv
     
-    def magnetization(self):
+    def magnetization(self,beta):
         m=np.sum(self.state)/float(self.L**self.dim)
         m2=np.sum(np.square(self.state))/float(self.L**self.dim)
-        chi=m2-m**2
+        chi=beta*(m2-m**2)
         return m,chi  
 
 def AutoCorrelation(histogram,method="integral"):
