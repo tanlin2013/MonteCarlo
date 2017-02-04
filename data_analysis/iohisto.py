446f1e1187f2ad92ac165d39d3bf8667882ce82d
Tan Tao-Lin <tanlin2013@gmail.com>
2017-02-04 17:41:33 +0800

    Update iohisto.py

--- data_analysis/iohisto.py
+++ data_analysis/iohisto.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-def save(histo,path):
-    
-    return
+def save(path,histo):
+    np.savez(path,*histo)
+    return None
     
 def read(path):
-    
+    npzfile=np.load(path)
+    histo=[npzfile[i] for i in npzfile]
+    npzfile.close() ; histo.reverse()
     return histo
